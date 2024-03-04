ac3e32ca50ab1e22fb992b54f324c16b87da81f3
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-03-04 09:24:02 +0100

    added csrfToken in middleware

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { v4 as uuidv4 } from 'uuid'
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next()
+
+  if (request.cookies.get('csrfToken')) {
+    return
+  }
+
+  const csrfToken = uuidv4()
+
+  response.cookies.set({
+    name: 'csrfToken',
+    value: csrfToken,
+    httpOnly: true,
+    secure: true,
+  })
+
+  response.headers.append('csrfToken', csrfToken)
+
+  return response
+}
