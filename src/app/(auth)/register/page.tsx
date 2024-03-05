34556affdf88fbb2d47ff06ef906bd9ea287ecac
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-03-05 18:10:20 +0100

    added input and label

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,10 +3,21 @@ import React from 'react'
 function SignUpPage() {
   return (
     <main>
-      <div className=''>
-        <h1 className="text-2xl font-semibold tracking-tight">
+      <div className="rounded-md shadow-md w-11/12 mx-auto max-w-xl mt-10 border p-2">
+        <h1 className="text-2xl font-semibold tracking-tight text-center">
           Create an account
         </h1>
+        
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
+        </div>
       </div>
     </main>
   )
