8548930a4d8ea8d0b6701cf407f6ce2f6f65359b
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-03-06 15:32:27 +0100

    firebase sing up client setup

---
+++ src/lib/firebase/create-user-with-email-and-password.ts
@@ -0,0 +1,18 @@
+import {
+  createUserWithEmailAndPassword as _createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from 'firebase/auth'
+import { auth } from './firebase-client'
+
+async function createUserWithEmailAndPassword(email: string, password: string) {
+  const firebaseResult = await _createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  )
+  await sendEmailVerification(firebaseResult.user)
+  auth.signOut()
+  return firebaseResult
+}
+
+export default createUserWithEmailAndPassword
