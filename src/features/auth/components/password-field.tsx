21157e511773899b6c71ae2fe5cef3e10c6f3a8f
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-03-06 10:02:48 +0100

    password filed

---
+++ src/features/auth/components/password-field.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { useFormContext } from 'react-hook-form'
+import { Input } from '@/components/ui/input'
+import InputPassword from '@/components/ui/input-password'
+
+type Props = {
+  name?: string
+}
+
+function PasswordField({ name = 'password' }: Props) {
+  const form = useFormContext()
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Password</FormLabel>
+          <FormControl>
+            <InputPassword placeholder={placeholder} {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+export const placeholder = 's1cretPassw0rd'
+
+export default PasswordField
