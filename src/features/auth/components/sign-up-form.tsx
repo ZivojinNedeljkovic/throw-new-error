cf6c0dc543d503e69af3fd3f523ad87e16e9583b
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-03-06 18:58:52 +0100

    added firebase email and password sign up

--- src/features/auth/components/sign-up-form.tsx
+++ src/features/auth/components/sign-up-form.tsx
@@ -1,16 +1,72 @@
+'use client'
+import { Form } from '@/components/ui/form'
 import React from 'react'
-import EmailAndPasswordSignUpForm from './email-and-password-sign-up-form'
-import Divider from './divider'
-import SignInWithGithub from './sign-in-with-github'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import EmailField from './email-field'
+import PasswordField from './password-field'
+import { Button } from '@/components/ui/button'
+import { zodResolver } from '@hookform/resolvers/zod'
+import ConfirmPasswordField from './confirm-password-field'
 
 function SignUpForm() {
+  const form = useForm<FormSchema>({
+    defaultValues: {
+      confirmPassword: '',
+    },
+    resolver: zodResolver(formSchema),
+  })
+
+  const onSubmit = form.handleSubmit(async ({ email, password }) => {
+    const signUpHandler = (
+      await import('@/lib/firebase/create-user-with-email-and-password')
+    ).default
+
+    try {
+      await signUpHandler(email, password)
+    } catch (error: any) {
+      if (error.code === 'auth/email-already-in-use') {
+        form.setError('email', {
+          message: 'Account already exists whit given email.',
+        })
+        return
+      }
+      return
+    }
+  })
+
   return (
-    <div className="flex flex-col gap-3">
-      <EmailAndPasswordSignUpForm />
-      <Divider />
-      <SignInWithGithub />
-    </div>
+    <Form {...form}>
+      <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+        <EmailField />
+        <PasswordField />
+        <ConfirmPasswordField />
+        <Button className="w-full mt-1" type="submit">
+          Register
+        </Button>
+      </form>
+    </Form>
   )
 }
+
+const formSchema = z
+  .object({
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(8, 'Must contain at least 8 character(s)')
+      .max(30, 'Must contain at most 30 character(s)'),
+    confirmPassword: z.string(),
+  })
+  .refine(
+    ({ password, confirmPassword }) =>
+      confirmPassword.length === 0 || password === confirmPassword,
+    {
+      message: "Passwords don't match.",
+      path: ['confirmPassword'],
+    }
+  )
+
+type FormSchema = z.infer<typeof formSchema>
 
 export default SignUpForm
