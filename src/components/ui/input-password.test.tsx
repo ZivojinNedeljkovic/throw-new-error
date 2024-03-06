ad420890f20ee4e953be150d0d4a725eab5e7ed1
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-03-06 18:36:45 +0100

    input password test

--- src/components/ui/input-password.test.tsx
+++ src/components/ui/input-password.test.tsx
@@ -1,9 +1,7 @@
 import { expect, test } from 'vitest'
 import InputPassword from './input-password'
-import { render, screen, act } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { resolve } from 'path'
-import { rejects } from 'assert' 
 
 test('test show password toggle button', async () => {
   render(<InputPassword />)
@@ -13,10 +11,6 @@ test('test show password toggle button', async () => {
   expect(passwordInput.getAttribute('type')).toBe('password')
 
   const toggleButton = screen.getByRole('button', { name: /show password/i })
-
-  expect(screen.getByText(/show password/i)).toBeInTheDocument()
-
-  await userEvent.hover(toggleButton)
 
   await userEvent.click(toggleButton)
 
