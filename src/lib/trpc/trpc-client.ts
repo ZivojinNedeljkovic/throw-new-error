2471daf5dc5fecb4951b44029135d35bd0d63944
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-03-08 15:12:49 +0100

    trpc setup

---
+++ src/lib/trpc/trpc-client.ts
@@ -0,0 +1,26 @@
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
+import { AppRouter } from './routers/_app'
+
+function getBaseUrl() {
+  if (typeof window !== 'undefined')
+    // browser should use relative path
+    return ''
+  if (process.env.VERCEL_URL)
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`
+  if (process.env.RENDER_INTERNAL_HOSTNAME)
+    // reference for render.com
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 5001}`
+}
+
+const trpc = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: `${getBaseUrl()}/api/trpc`,
+    }),
+  ],
+})
+
+export default trpc
