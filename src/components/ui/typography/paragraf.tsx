22a6330b8d92cc141041d2fd875554b17d2768a5
Zivojin Nedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-03-08 15:46:15 +0100

    sing in form loading and errors

---
+++ src/components/ui/typography/paragraf.tsx
@@ -0,0 +1,31 @@
+import { cn } from '@/lib/utils'
+import { Slot } from '@radix-ui/react-slot'
+import { VariantProps, cva } from 'class-variance-authority'
+import React from 'react'
+
+const headingVariants = cva('font-sans scroll-m-20 ', {
+  variants: {
+    variant: {
+      error: 'text-base font-semibold text-destructive tracking-tight mb-4',
+    },
+  },
+})
+
+export type ParagraphProps = React.HTMLAttributes<HTMLParagraphElement> &
+  VariantProps<typeof headingVariants> & {
+    asChild?: boolean
+  }
+
+function Paragraph({
+  variant,
+  className,
+  asChild = false,
+  ...props
+}: ParagraphProps) {
+  const Comp = asChild ? Slot : 'div'
+  return (
+    <Comp {...props} className={cn(headingVariants({ variant, className }))} />
+  )
+}
+
+export default Paragraph
--- src/features/auth/components/sign-in-form.tsx
+++ src/features/auth/components/sign-in-form.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Form } from '@/components/ui/form'
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import EmailField from './email-field'
@@ -9,6 +9,8 @@ import { Button } from '@/components/ui/button'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useAppDispatch } from '@/lib/redux/hooks'
 import thunkSignInUserWithEmailAndPassword from '@/features/store/thunk-sign-in-user-with-email-and-password'
+import SpinnerIcon from '@/components/ui/icons/spinner-icon'
+import Paragraph from '@/components/ui/typography/paragraf'
 
 function SignInForm() {
   const form = useForm<FormSchema>({
@@ -19,18 +21,50 @@ function SignInForm() {
     resolver: zodResolver(formSchema),
   })
 
+  const [isLoading, setIsLoading] = useState(false)
+
   const dispatch = useAppDispatch()
 
   const onSubmit = form.handleSubmit(async ({ email, password }) => {
-    await dispatch(thunkSignInUserWithEmailAndPassword(email, password))
+    setIsLoading(true)
+    try {
+      throw new Error('test error')
+
+      await dispatch(thunkSignInUserWithEmailAndPassword(email, password))
+    } catch (error: any) {
+      switch (error.code) {
+        case 'auth/user-not-found':
+          form.setError('email', {
+            message: 'User not found.',
+          })
+          break
+        case 'auth/wrong-password':
+          form.setError('password', { message: 'Wrong password.' })
+          break
+        default:
+          form.setError('root', {
+            message: 'Unexpected error occurred.',
+          })
+          break
+      }
+    }
+    setIsLoading(false)
   })
+
+  const rootError = form.formState.errors.root?.message
 
   return (
     <Form {...form}>
       <form className="flex flex-col gap-4" onSubmit={onSubmit}>
         <EmailField />
         <PasswordField />
-        <Button className="w-full mt-1" type="submit">
+        {rootError && (
+          <Paragraph variant="error" className="m-0">
+            {rootError}
+          </Paragraph>
+        )}
+        <Button className="w-full mt-1" type="submit" disabled={isLoading}>
+          {isLoading && <SpinnerIcon className="mr-2 h-4 w-4 animate-spin" />}
           Sing in
         </Button>
       </form>
