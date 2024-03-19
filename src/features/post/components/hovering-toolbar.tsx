91860f42c23c19773eba26f5f9e4c0df1ae62db4
ZivojinNedeljkovic <zivojin.nedeljkovic.pa@gmail.com>
2024-03-19 18:57:30 +0100

    hover tooltip update

--- src/features/post/components/hovering-toolbar.tsx
+++ src/features/post/components/hovering-toolbar.tsx
@@ -4,6 +4,15 @@ import ClientOnly from '@/components/client-only'
 import { Button } from '@/components/ui/button'
 import usePositionOnSelected from '../hooks/use-position-on-selected'
 import { TriangleDownIcon } from '@radix-ui/react-icons'
+import { Transition, TransitionStatus } from 'react-transition-group'
+
+const transitionStyles: Partial<Record<TransitionStatus, React.CSSProperties>> =
+  {
+    entering: { opacity: 1 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0 },
+  }
 
 function HoveringToolbar() {
   const { ref, top, left, isTextSelected } = usePositionOnSelected()
@@ -11,24 +20,33 @@ function HoveringToolbar() {
   return (
     <ClientOnly>
       <Portal.Root>
-        {isTextSelected && (
-          <div
-            className="absolute z-10 transition-opacity"
-            style={{ top, left }}
-          >
-            <menu ref={ref} className="bg-primary text-white rounded-md">
-              <Button
-                onMouseDown={() => {
-                  console.log('click')
-                }}
-              >
-                B
-              </Button>
-              <Button>I</Button>
-            </menu>
-            <TriangleDownIcon className="-mt-[6.1px] mx-auto" />
-          </div>
-        )}
+        <Transition
+          nodeRef={ref}
+          in={isTextSelected}
+          timeout={850}
+          mountOnEnter
+          unmountOnExit
+          appear
+        >
+          {state => (
+            <div
+              className="absolute z-10 transition-opacity delay-700"
+              style={{ top, left, ...transitionStyles[state] }}
+            >
+              <menu ref={ref} className="bg-primary text-white rounded-md">
+                <Button
+                  onMouseDown={() => {
+                    console.log('click')
+                  }}
+                >
+                  B
+                </Button>
+                <Button>I</Button>
+              </menu>
+              <TriangleDownIcon className="-mt-[6.1px] mx-auto" />
+            </div>
+          )}
+        </Transition>
       </Portal.Root>
     </ClientOnly>
   )
